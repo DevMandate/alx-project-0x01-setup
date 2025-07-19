d0e5707633298ac8abd379956d77a3f24833be1d
DevMandate <wafulacaleb233@gmail.com>
2025-07-19 22:34:01 +0300

    Add header and navigation between pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,6 +1,9 @@
+import Header from "@/components/layout/Header";
+
 const PostsPage = () => {
   return (
     <div className="p-4">
+      <Header />
       <h1 className="text-3xl font-bold">Posts</h1>
     </div>
   );
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,6 +1,10 @@
+import Header from "@/components/layout/Header";
+
+
 const UsersPage = () => {
   return (
     <div className="p-4">
+      <Header />
       <h1 className="text-3xl font-bold">Users</h1>
     </div>
   );
